4efb88c7eeb9f2b9ecc5f001465d5b3389f18824
papantonioul <papantonioul@unisystems.gr>
2024-06-17 14:19:17 +0300

    implementation of get country by id method

--- src/main/java/com/example/nations/service/CountryService.java
+++ src/main/java/com/example/nations/service/CountryService.java
@@ -2,13 +2,18 @@ package com.example.nations.service;
 
 import com.example.nations.domain.Country;
 import com.example.nations.repository.CountryRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CountryService {
     private final CountryRepository countryRepository;
+
+    private final Logger log = LoggerFactory.getLogger(CountryService.class);
 
     public CountryService(CountryRepository countryRepository) {
         this.countryRepository = countryRepository;
@@ -18,4 +23,19 @@ public class CountryService {
         return countryRepository.findAll();
     }
 
+
+    /**
+     * Get country by id
+     *
+     * @param id the id of the country
+     * @return the country with the specific id
+     */
+    public Country getCountryById(Long id) {
+        log.debug("Rest request to get a country by its id ");
+        Optional<Country> country = countryRepository.findById(id);
+        if (country.isEmpty()) {
+            throw new RuntimeException("Country not found with id ");
+        }
+        return country.get();
+    }
 }
--- src/main/java/com/example/nations/web/CountryController.java
+++ src/main/java/com/example/nations/web/CountryController.java
@@ -3,6 +3,7 @@ package com.example.nations.web;
 import com.example.nations.domain.Country;
 import com.example.nations.service.CountryService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,16 @@ public class CountryController {
     @GetMapping
     public List<Country> getAllCountries() {
         return countryService.getAllCountries();
+    }
+
+    /**
+     * Get country by id
+     *
+     * @param id the id of the country
+     * @return the country
+     */
+    @GetMapping("/{id}")
+    public Country getCountryById(@PathVariable Long id) {
+        return countryService.getCountryById(id);
     }
 }
